485f2004df0945377d6d8e0c904eef003a9a7575
L1STOV <listovspartak99@gmail.com>
2020-09-23 14:39:38 +0300

    Add data to transfers table

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ connection = psycopg2.connect(
 
 cur = connection.cursor()
 
+cur.execute(
+    "INSERT INTO transfers(date, player_first_name, player_second_name, transfer_type, from_club, to_club, price) "
+    "VALUES ('2020-07-01', 'Cedric', 'Soares', 'Free transfer', 'Southampton', 'Arsenal', 'free');"
+)
+
 connection.commit()
 
 print("-- Data insert --")
