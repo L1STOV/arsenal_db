de5e0f8b3e6b36b1fdabf3c2eca055b0886a1ee1
L1STOV <listovspartak99@gmail.com>
2020-09-23 13:37:15 +0300

    Create table 'transfers'

--- main.py
+++ main.py
@@ -8,5 +8,19 @@ connection = psycopg2.connect(
     port="5432"
 )
 
-print("Database opened")
-cursor = connection.cursor()
+cur = connection.cursor()
+
+cur.execute('''CREATE TABLE transfers(
+id BIGSERIAL PRIMARY KEY,
+date DATE NOT NULL,
+player_first_name VARCHAR(25) NOT NULL,
+player_second_name VARCHAR(25) NOT NULL,
+transfer_type VARCHAR(25) NOT NULL,
+from_club VARCHAR(25) NOT NULL,
+to_club VARCHAR(25) NOT NULL,
+price VARCHAR(30)
+);
+''')
+
+connection.commit()
+connection.close()
